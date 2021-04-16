1014b3bb54a6d9851b9c7fe8314db5296c1f3a56
Michael Stevan Lapandio <michael.lapandio@xapiens.id>
2021-04-16 09:00:16 +0700

    [kyomel] done 1

--- myApp.js
+++ myApp.js
@@ -1,7 +1,17 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let Person;
+const Schema = mongoose.Schema;
+
+let personSchema = new Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String]
+});
+
+let Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
